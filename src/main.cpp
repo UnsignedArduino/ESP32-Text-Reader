6565c53f1de7f648136263ed0442a0c238b51380
UnsignedArduino <UnsignedArduino@outlook.com>
2022-01-12 17:30:40 -0500

    Can blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
 }
