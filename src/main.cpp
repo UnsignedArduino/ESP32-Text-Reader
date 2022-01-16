5413b89014948d49d1a4cd9042cacb8954a204a0
UnsignedArduino <UnsignedArduino@outlook.com>
2022-01-16 13:11:34 -0500

    Can read buttons

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,21 @@
 #include "utils.h"
 #include "sdUtils.h"
 #include "epaperUtils.h"
-#include "pinouts.h"
+#include "buttonUtils.h"
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
   Serial.println();
 
-  if (beginSD(sdCS) != 0) {
+  if (beginSD() != 0) {
     blinkError(100, 400);
   }
   if (beginEPaper() != 0) {
     blinkError(250, 250);
   }
+
+  beginButtons();
 }
 
 void loop() {
