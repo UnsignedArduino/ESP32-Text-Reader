5958bc64db0161f51b10e3873bad7ac794920085
UnsignedArduino <UnsignedArduino@outlook.com>
2022-01-16 14:27:04 -0500

    Can print contents of text file to screen

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,48 @@
 #include <GUI_Paint.h>
 #include "buttonUtils.h"
 
+File file;
+unsigned long filePos = 0;
+
+const byte maxCharPerLine = 42;
+const byte maxLines = 30;
+
+void printFromLocation() {
+  Paint_Clear(WHITE);
+
+  Serial.print("Reading from location ");
+  Serial.println(file.position());
+
+  Serial.println("Loading from file");
+  Serial.println("Got: ");
+  for (unsigned int yPos = 0; yPos < EPD_4IN2_WIDTH; yPos += 13) {
+    char line[maxCharPerLine + 1] = "";
+    byte i = 0;
+    for ( ; i < maxCharPerLine; i ++) {
+      int nextByte = file.read();
+      if (nextByte == -1) {
+        break;
+      }
+      char ch = (char)nextByte;
+      if (ch == '\n') { // \n
+        break;
+      } else if (ch == '\r') {  // \r\n
+        file.read();  // Advance forward one more because we still have \n to consume
+        break;
+      } else {
+        line[i] = ch;
+      }
+    }
+    line[i] = '\0';
+    Serial.println(line);
+
+    Paint_DrawString_EN(0, yPos, line, &Font12, WHITE, BLACK);
+  }
+
+  Serial.println("Showing contents");
+  EPD_4IN2_Display(imageBuffer);
+}
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
@@ -20,8 +62,17 @@ void setup() {
 
   beginButtons();
 
-  Paint_DrawString_EN(0, 0, "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?", &Font12, WHITE, BLACK);
-  EPD_4IN2_Display(imageBuffer);
+  Serial.println("Opening /bible.txt");
+  file = SD.open("/bible.txt", FILE_READ);
+  if (!file) {
+    Serial.println("Unable to open /bible.txt");
+    blinkError(500, 1000);
+  }
+  
+  filePos = 0;
+  file.seek(filePos);
+  printFromLocation();
+  file.close();
 }
 
 void loop() {
