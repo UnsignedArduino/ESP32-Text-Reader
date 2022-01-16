57e7b7d74fcd2574e278958641b37e88d8540ccd
UnsignedArduino <UnsignedArduino@outlook.com>
2022-01-16 13:15:18 -0500

    Allocate image buffer for EPaper display

--- src/epaperUtils.h
+++ src/epaperUtils.h
@@ -1,3 +1,7 @@
 #include <Arduino.h>
+#include "EPD.h"
+
+const unsigned int bwImageSize = ((EPD_4IN2_WIDTH % 8 == 0) ? (EPD_4IN2_WIDTH / 8 ) : (EPD_4IN2_WIDTH / 8 + 1)) * EPD_4IN2_HEIGHT;
+byte imageBuffer[bwImageSize] = {};
 
 int beginEPaper();
